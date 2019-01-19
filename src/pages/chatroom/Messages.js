8f973c0fa0089ec794473c3fe647142d655024f3
Enes <enesozturk.d@gmail.com>
2019-01-19 18:12:05 +0300

    top bar and input container design for  messages component

--- src/pages/chatroom/Messages.js
+++ src/pages/chatroom/Messages.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import Call from 'react-icons/lib/md/call'
+import VideoCall from 'react-icons/lib/md/video-call'
+
 export class Messages extends Component {
 
     render() {
         return (
             <div className="messages">
+                <div className="top-bar">
+                    <div className="username">
+                        <span>Berkay Akbulut</span>
+                    </div>
+                    <div className="user-call-icons">
+                        <Call size={30} />
+                        <VideoCall size={30} />
+                    </div>
+                </div>
                 <div className="message-list">
 
                 </div>
-                <div className="message-input-container">
-                    <input type="text" className="message-input" />
+                <div className="input-container">
+                    <input type="text" placeholder="Type your message here..." className="message-input" />
                 </div>
             </div>
         )
