0519bedb5bfeef0f4e8003747850d10c1990c390
Enes <enesozturk.d@gmail.com>
2019-01-20 14:06:11 +0300

    redesign message list with scaleble height depends on message count

--- src/pages/chatroom/components/messages/MessageList.js
+++ src/pages/chatroom/components/messages/MessageList.js
@@ -5,7 +5,68 @@ export default class MessageList extends Component {
         return (
             <div className="message-list">
                 <div className="message from-you">
-                    <img src="./static/images/irem.jpg" alt="" />
+                    <div className="content-wrapper">
+                        <img src="./static/images/irem.jpg" alt="" />
+                        <div className="message-container">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit.
+                        </p>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit.
+                        </p>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Commodi blanditiis
+                                necessitatibus quod rem, accusamus facere,
+                                earum harum soluta molestiae magnam
+                                sed nobis similique officiis
+                                repudiandae itaque quos quia
+                                mollitia dolorem.
+                        </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="message from-me">
+                    <div className="content-wrapper">
+                        <div className="message-container">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Commodi blanditiis
+                                necessitatibus quod rem, accusamus facere,
+                                earum harum soluta molestiae magnam
+                                sed nobis similique officiis
+                                repudiandae itaque quos quia
+                                mollitia dolorem.
+                        </p>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit.
+                        </p>
+                        </div>
+                        <img src="./static/images/enes.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="message from-you">
+                    <div className="content-wrapper">
+                        <img src="./static/images/irem.jpg" alt="" />
+                        <div className="message-container">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Commodi blanditiis
+                                necessitatibus quod rem, accusamus facere,
+                                earum harum soluta molestiae magnam
+                                sed nobis similique officiis
+                                repudiandae itaque quos quia
+                                mollitia dolorem.
+                        </p>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="message from-me">
                     <div className="message-container">
                         <p>
                             Lorem ipsum dolor sit amet consectetur
@@ -16,18 +77,9 @@ export default class MessageList extends Component {
                             repudiandae itaque quos quia
                             mollitia dolorem.
                         </p>
-                    </div>
-                </div>
-                <div className="message from-me">
-                    <div className="message-container">
                         <p>
                             Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Commodi blanditiis
-                            necessitatibus quod rem, accusamus facere,
-                            earum harum soluta molestiae magnam
-                            sed nobis similique officiis
-                            repudiandae itaque quos quia
-                            mollitia dolorem.
+                            adipisicing elit.
                         </p>
                     </div>
                     <img src="./static/images/enes.jpg" alt="" />
@@ -44,33 +96,12 @@ export default class MessageList extends Component {
                             repudiandae itaque quos quia
                             mollitia dolorem.
                         </p>
-                    </div>
-                </div>
-                <div className="message from-me">
-                    <div className="message-container">
                         <p>
                             Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Commodi blanditiis
-                            necessitatibus quod rem, accusamus facere,
-                            earum harum soluta molestiae magnam
-                            sed nobis similique officiis
-                            repudiandae itaque quos quia
-                            mollitia dolorem.
+                            adipisicing elit.
                         </p>
-                    </div>
-                    <img src="./static/images/enes.jpg" alt="" />
-                </div>
-                <div className="message from-you">
-                    <img src="./static/images/irem.jpg" alt="" />
-                    <div className="message-container">
                         <p>
-                            Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Commodi blanditiis
-                            necessitatibus quod rem, accusamus facere,
-                            earum harum soluta molestiae magnam
-                            sed nobis similique officiis
-                            repudiandae itaque quos quia
-                            mollitia dolorem.
+                            Lorem ipsum dolor sit amet.
                         </p>
                     </div>
                 </div>
@@ -88,6 +119,7 @@ export default class MessageList extends Component {
                     </div>
                     <img src="./static/images/enes.jpg" alt="" />
                 </div>
+                 */}
             </div>
         )
     }
